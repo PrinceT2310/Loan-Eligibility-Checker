7c2f3d3b0d96a8454d00bbc47200d7376941360e
PrinceT2310 <princethakur64365@gmail.com>
2026-04-30 17:37:05 +0530

    fix: cors allow localhost and vercel both

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,11 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: [
+    "https://loan-eligibility-checker-4rmw.vercel.app",
+    "http://localhost:5173",
+    "http://localhost:5174",
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
